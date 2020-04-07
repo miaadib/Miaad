49354b12047104889c6ef551e2bbd7d693d6cc0e
miaadibab@outlook.com <miaadibab@outlook.com>
2020-04-08 00:08:50 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            int w = 2;
+            int @sum;
+            @sum = i + w;
+            Console.WriteLine(@sum);
+            Console.WriteLine("hello Wolrd");
+            Console.WriteLine("hello Wolrd");
+            Console.Read();
+
+        }
+    }
+}
